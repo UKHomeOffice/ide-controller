6b87d61342f70d731bd378653d6370245025d1aa
Richard Weeks <richard.weeks@viabledata.co.uk>
2020-08-13 16:34:05 +0100

    Display photo.

---
+++ src/Types/DocumentData.js
@@ -0,0 +1,11 @@
+
+export default class DocumentData {
+    
+    constructor(data, codelineData, image) {
+        this.data = data;
+        this.codelineData = codelineData;
+        this.image = image;
+    }
+
+
+}
