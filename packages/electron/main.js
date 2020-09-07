4e87a27fae8e5f9ed20a1694c6a639ee3e2db0d2
Ali Ali <alialbaker2007@gmail.com>
2020-09-07 19:24:06 +0100

    Add electron script

---
+++ packages/electron/main.js
@@ -0,0 +1,45 @@
+// Modules
+const {app, BrowserWindow, screen} = require('electron')
+const path = require('path');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+// Create a new BrowserWindow when `app` is ready
+function createWindow () {
+
+  mainWindow = new BrowserWindow({
+    // frame: false,
+    width: 1800,
+    height: 1200,
+    resizable: false,
+    titleBarStyle: 'hidden',
+    webPreferences: { nodeIntegration: true }
+  })
+
+
+  // Load index.html into the new BrowserWindow
+  mainWindow.loadURL('http://localhost:3000')
+
+  // Open DevTools - Remove for PRODUCTION!
+  // mainWindow.webContents.openDevTools();
+
+  // Listen for window being closed
+  mainWindow.on('closed',  () => {
+    mainWindow = null
+  })
+}
+
+// Electron `app` is ready
+app.on('ready', createWindow)
+
+// Quit when all windows are closed - (Not macOS - Darwin)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// When app icon is clicked and app is running, (macOS) recreate the BrowserWindow
+app.on('activate', () => {
+  if (mainWindow === null) createWindow()
+})
