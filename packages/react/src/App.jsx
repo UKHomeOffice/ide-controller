5b8f675fc9647be5b53b02f7dafacf9673086ee0
Ali Ali <alialbaker2007@gmail.com>
2020-09-07 14:03:18 +0100

    Remove unused packages

--- packages/react/src/App.jsx
+++ packages/react/src/App.jsx
@@ -1,6 +1,5 @@
 // Global imports
 import React, { useState, useEffect } from 'react';
-import { logger } from 'react-native-logs';
 
 // Local  imports
 import './App.scss';
@@ -9,8 +8,6 @@ import Header from './Components/Header';
 import PageBody from './Components/PageBody';
 import DocumentData from './Types/DocumentData';
 import { ImageProvider } from './Components/ImageContext';
-
-const log = logger.createLogger();
 
 const App = () => {
   const [fullpage, setFullpage] = useState(new Map());
@@ -34,14 +31,11 @@ const App = () => {
 
       events.addEventListener('event', (e) => {
         const messageData = JSON.parse(e.data);
-        log.info(`EVENT ${messageData.dataType} Event ${messageData.event}`);
         if (messageData.event === 'START_OF_DOCUMENT_DATA') {
-          log.info('START OF DOCUMENT');
           setFullpage(fullpage.clear());
         }
 
         if (messageData.event === 'END_OF_DOCUMENT_DATA') {
-          log.info('END OF DOCUMENT');
           setFullpage(fullpage);
         }
       });
