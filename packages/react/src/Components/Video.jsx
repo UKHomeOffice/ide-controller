c9b0449b73fe1cdc3f9600771465732ed81e4805
Ali Ali <alialbaker2007@gmail.com>
2020-09-08 16:33:12 +0100

    Add catch to navigator.mediaDevices

--- packages/react/src/Components/Video.jsx
+++ packages/react/src/Components/Video.jsx
@@ -6,23 +6,26 @@ import Config from './Config';
 
 const Video = forwardRef(({ updateRef, style, captureOptions }, videoRef) => {
   const [mediaStream, setMediaStream] = useState(null);
-  navigator.mediaDevices.getUserMedia(captureOptions).then((stream) => {
-    setMediaStream(stream);
+  navigator.mediaDevices
+    .getUserMedia(captureOptions)
+    .then((stream) => {
+      setMediaStream(stream);
 
-    if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
-      videoRef.current.srcObject = stream;
-      videoRef.current.play();
-      videoRef.current.addEventListener(
-        'canplay',
-        () => {
-          videoRef.current.width = videoRef.current.videoWidth;
-          videoRef.current.height = videoRef.current.videoHeight;
-          updateRef({ ...videoRef });
-        },
-        false
-      );
-    }
-  });
+      if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
+        videoRef.current.srcObject = stream;
+        videoRef.current.play();
+        videoRef.current.addEventListener(
+          'canplay',
+          () => {
+            videoRef.current.width = videoRef.current.videoWidth;
+            videoRef.current.height = videoRef.current.videoHeight;
+            updateRef({ ...videoRef });
+          },
+          false
+        );
+      }
+    })
+    .catch(console.error);
 
   return mediaStream ? (
     <video
