0bc2af3762804f1fed33d16f11a6eb046e7a4880
Ali Ali <alialbaker2007@gmail.com>
2020-09-08 11:27:54 +0100

    Add dragable titleBar to electron

--- packages/react/src/Components/Header.jsx
+++ packages/react/src/Components/Header.jsx
@@ -1,9 +1,19 @@
 // Global imports
 import React from 'react';
+import style from 'styled-components';
+
+const StyledHeader = style.header`
+  -webkit-app-region: drag;
+  padding-top: 10px;
+`;
 
 export default function Header() {
   return (
-    <header className="govuk-header " role="banner" data-module="govuk-header">
+    <StyledHeader
+      className="govuk-header "
+      role="banner"
+      data-module="govuk-header"
+    >
       <div className="govuk-header__container govuk-width-container">
         <div className="govuk-header__content">
           <a
@@ -14,6 +24,6 @@ export default function Header() {
           </a>
         </div>
       </div>
-    </header>
+    </StyledHeader>
   );
 }
