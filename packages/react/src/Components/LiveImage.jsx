ba3077dd9970d9616fa8010133c960174120a8ee
Ali Ali <alialbaker2007@gmail.com>
2020-09-10 21:21:52 +0100

    More Refactor

--- packages/react/src/Components/LiveImage.jsx
+++ packages/react/src/Components/LiveImage.jsx
@@ -1,10 +1,9 @@
 // Global imports
 import React, { useRef, useEffect, useState } from 'react';
-import * as tf from '@tensorflow/tfjs'; // eslint-disable-line
-import * as posenet from '@tensorflow-models/posenet';
 
 // Local imports
 import Video from './Video';
+import { ResPosenet } from '../helpers';
 import './Controller.css';
 
 const CAPTURE_OPTIONS = {
@@ -16,63 +15,58 @@ const CAPTURE_OPTIONS = {
     acingMode: 'user',
   },
 };
-
-const estimateSinglePose = async (video, threshold = 0.99) => {
-  const net = await posenet.load({
-    architecture: 'ResNet50',
-    outputStride: 16,
-    inputResolution: {
-      width: CAPTURE_OPTIONS.video.width,
-      height: CAPTURE_OPTIONS.video.height,
-    },
-    quantBytes: 2,
-  });
-
-  // parameter(imageSource, imageScaleFactor, flipHorizontal, outputStride)
-  const pose = await net.estimateSinglePose(video, 0.5, false, 16);
-  const isBelowThreshold = !!pose.keypoints
-    .slice(0, 5)
-    .find((poseItem) => poseItem.score < threshold);
-  if (isBelowThreshold && !video.paused) {
-    setTimeout(() => estimateSinglePose(video), 200);
-  } else {
-    video.pause();
-    video.currentTime = 0;
-  }
-};
+const THRESHOLD = 0.99;
 
 const LiveImage = () => {
-  // const canvasRef = useRef();
+  const canvasRef = useRef();
   const [videoRef, setVideoRef] = useState(useRef());
-  // const [pose, setPose] = useState(null);
+  const [pose, setPose] = useState(null);
+
+  const updateCanvas = () => {
+    const ctx = canvasRef.current.getContext('2d');
+    ctx.drawImage(videoRef.current, 0, 0);
+    videoRef.current.style.display = 'none';
+  };
+  const estimateSinglePose = async () => {
+    const video = videoRef.current;
+    if (!video) return;
+    const net = await ResPosenet(
+      CAPTURE_OPTIONS.video.width,
+      CAPTURE_OPTIONS.video.height
+    );
+    // parameter(imageSource, imageScaleFactor, flipHorizontal, outputStride)
+    const singlePose = await net.estimateSinglePose(video, 0.5, false, 16);
+    const isBelowThreshold = !!singlePose.keypoints
+      .slice(0, 5)
+      .find((poseItem) => poseItem.score < THRESHOLD);
+    if (isBelowThreshold) {
+      estimateSinglePose();
+    } else {
+      updateCanvas();
+      video.pause();
+      setPose(singlePose);
+    }
+  };
 
   useEffect(() => {
     if (!videoRef.current) return;
-    videoRef.current.addEventListener('canplay', () =>
-      estimateSinglePose(videoRef.current)
-    );
+    videoRef.current.addEventListener('canplay', estimateSinglePose);
   });
 
   return (
     <div className="govuk-grid-column-one-third">
       <div className="photoContainer--photo medium at6">
-        {pose ? (
-          <Video
-            ref={videoRef}
-            setVideoRef={setVideoRef}
-            captureOptions={CAPTURE_OPTIONS}
-          />
-        ) : (
-          {
-            /* <canvas
-          hidden
+        <Video
+          ref={videoRef}
+          setVideoRef={setVideoRef}
+          captureOptions={CAPTURE_OPTIONS}
+        />
+        <canvas
+          hidden={!pose}
           ref={canvasRef}
           width={CAPTURE_OPTIONS.video.width}
           height={CAPTURE_OPTIONS.video.height}
-          style={{ borderRadius: '10px 10px 0 0' }}
-        /> */
-          }
-        )}
+        />
       </div>
     </div>
   );
