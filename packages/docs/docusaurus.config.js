3be3ee0b982d5a9ecf67f7711abebc0c46fad29e
Ali Ali <alialbaker2007@gmail.com>
2020-09-14 22:29:31 +0100

    Add Docusaurus

---
+++ packages/docs/docusaurus.config.js
@@ -0,0 +1,80 @@
+module.exports = {
+  title: 'IDE Controller Documentation',
+  tagline: 'IDE Controller',
+  url: 'https://pages.github.com/UKHomeOffice/ide-controller',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'IDE Controller',
+      logo: {
+        alt: 'IDE Controller',
+        src: 'img/icon.png',
+      },
+      items: [
+        {
+          href: 'https://github.com/UKHomeOffice/ide-controller',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Style Guide',
+              to: 'docs/',
+            },
+            {
+              label: 'Second Doc',
+              to: 'docs/doc2/',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/viable-data',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/UKHomeOffice/ide-controller',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} IDE Controller. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
