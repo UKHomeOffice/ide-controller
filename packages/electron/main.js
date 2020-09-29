5073b1cb7d8163e493b97db89660e222e1af9d48
Ali Ali <alialbaker2007@gmail.com>
2020-09-29 08:44:13 +0100

    Import store in main.js

--- packages/electron/main.js
+++ packages/electron/main.js
@@ -12,6 +12,7 @@ const path = require('path');
 
 // Local imports
 const ideMenu = require('./menu');
+const Store = require('./store');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
