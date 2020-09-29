40a94f8977ad444263878216236ac7befba1717f
Ali Ali <alialbaker2007@gmail.com>
2020-09-29 13:53:44 +0100

    Update Image atom

--- packages/react/src/Components/Atoms/Image.jsx
+++ packages/react/src/Components/Atoms/Image.jsx
@@ -1,31 +1,20 @@
 // Global imports
+import PropTypes from 'prop-types';
 import React from 'react';
 
 // Local imports
-import { ImageConsumer } from '../ImageContext';
-import Config from '../Config';
 import '../Controller.scss';
 
-const constructImageURL = (base64) => `data:image/jpeg;base64,${base64}`;
-
-const Image = (imageID, imageAlt) => {
+const Image = ({ image, imageAlt }) => {
   return (
-    <ImageConsumer>
-      {(fullPage) => {
-        const datamap = new Map(fullPage);
-        const image = datamap.has(imageID)
-          ? constructImageURL(datamap.get(imageID).image)
-          : Config.blankAvatar;
-        return (
-          <div
-            className="photoContainer--photo medium"
-            alt={imageAlt}
-            style={{ backgroundImage: `url(${image})` }}
-          />
-        );
-      }}
-    </ImageConsumer>
+    <div
+      className="photoContainer--photo medium"
+      alt={imageAlt}
+      style={{ backgroundImage: `url(${image})` }}
+    />
   );
 };
+
+// @todo add prop checks and defaults
 
 export default Image;
