b852de29e4fbeb800439a1f6d2e2fb1f1f70eb4e
James <jpoulten@me.com>
2020-09-30 11:13:43 +0100

    Refactor image panel

---
+++ packages/react/src/Components/DocumentImage.jsx
@@ -0,0 +1,27 @@
+// Global imports
+import PropTypes from 'prop-types';
+import React from 'react';
+
+// Local imports
+import { Column } from './Layout';
+import ImageCard from './Molecules/ImageCard';
+
+const DocumentImage = ({ image, imageAlt }) => {
+  return (
+    <Column size="one-third padding-5">
+      <ImageCard image={image} imageAlt={imageAlt} />
+    </Column>
+  );
+};
+
+DocumentImage.propTypes = {
+  image: PropTypes.string,
+  imageAlt: PropTypes.string,
+};
+
+DocumentImage.defaultProps = {
+  image: '',
+  imageAlt: '',
+};
+
+export default DocumentImage;
