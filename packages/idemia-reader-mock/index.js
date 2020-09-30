66305e1d6432cfab8305482ffdc76d5291a1a0e3
Ali Ali <alialbaker2007@gmail.com>
2020-09-30 14:43:13 +0100

    Add mock

---
+++ packages/idemia-reader-mock/index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const response = require('./response.json');
+
+const server = http.createServer((req, res) => {
+  console.log(req.url)
+  if (req.method === 'POST' && req.url === '/reader/data') {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(response));
+  }
+  res.statusCode = 404;
+  res.end('Not found!')
+});
+server.listen(8081);
