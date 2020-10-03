6376fab91443f2f80a8e6f301420557821df4bbd
Ali Ali <alialbaker2007@gmail.com>
2020-10-03 18:01:25 +0100

    Add disabled to button

--- packages/react/src/Components/Atoms/Button.jsx
+++ packages/react/src/Components/Atoms/Button.jsx
@@ -2,13 +2,14 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Button = ({ buttonVariant, onClick, children }) => {
+const Button = ({ buttonVariant, onClick, children, disabled }) => {
   return (
     <button
       className={`govuk-button govuk-button--${buttonVariant}`}
       data-module="govuk-button"
       type="button"
       onClick={onClick}
+      disabled={disabled}
     >
       {children}
     </button>
@@ -19,11 +20,13 @@ Button.propTypes = {
   children: PropTypes.string.isRequired,
   buttonVariant: PropTypes.string,
   onClick: PropTypes.func,
+  disabled: PropTypes.bool,
 };
 
 Button.defaultProps = {
   buttonVariant: 'secondary',
   onClick: null,
+  disabled: false,
 };
 
 export default Button;
