1f1f1d8197e8713938d1464b01eaa775aa76f9d9
Ali Ali <alialbaker2007@gmail.com>
2020-10-05 23:04:45 +0100

    Fix conflict

--- packages/react/src/Components/Atoms/TableRow.jsx
+++ packages/react/src/Components/Atoms/TableRow.jsx
@@ -1,28 +1,39 @@
 // Global imports
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 
-const TableRow = ({ rowLabel, status }) => {
+const TableRow = ({ rowLabel, status, type }) => {
   return (
     <tr className="govuk-table__row">
       <th
-        className="govuk-table__cell govuk-!-font-weight-regular govuk-!-width-one-half"
+        className={`govuk-table__${type} govuk-!-font-weight-regular govuk-!-width-one-half`}
         scope="row"
       >
         {rowLabel}
       </th>
-      <td className="govuk-table__cell govuk-!-width-one-half">
-        <strong className="govuk-tag govuk-tag--passed app-task-list__task-completed govuk-!-font-size-16">
-          {status}
-        </strong>
-      </td>
+      {status && (
+        <td className="govuk-table__cell">
+          <strong
+            className="govuk-tag govuk-tag--failed app-task-list__task-completed"
+            id="eligibility-completed"
+          >
+            {status}
+          </strong>
+        </td>
+      )}
     </tr>
   );
 };
 
 TableRow.propTypes = {
   rowLabel: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired,
+  status: PropTypes.string,
+  type: PropTypes.string,
+};
+
+TableRow.defaultProps = {
+  status: undefined,
+  type: 'cell',
 };
 
 export default TableRow;
