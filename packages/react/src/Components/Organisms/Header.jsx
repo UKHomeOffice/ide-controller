7a8128bb50397772f79f3adf9e19222a9bc092c1
Ali Ali <alialbaker2007@gmail.com>
2020-10-05 08:45:48 +0100

    Fix drababble

--- packages/react/src/Components/Organisms/Header.jsx
+++ packages/react/src/Components/Organisms/Header.jsx
@@ -2,7 +2,11 @@
 import React from 'react';
 
 const Header = () => (
-  <header className="govuk-header" role="banner" data-module="govuk-header">
+  <header
+    className="govuk-header dragable"
+    role="banner"
+    data-module="govuk-header"
+  >
     <div className="govuk-header__container govuk-width-container">
       <div className="govuk-header__content">
         <span className="govuk-header__link govuk-header__link--service-name">
