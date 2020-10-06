cea8c091f336c68179112d7fa71d79c22e954be6
Ali Ali <alialbaker2007@gmail.com>
2020-10-06 12:21:24 +0100

    Add header application/json

--- packages/react/src/helpers/common.js
+++ packages/react/src/helpers/common.js
@@ -8,6 +8,7 @@ export const post = (url, data) =>
   fetch(url, {
     method: 'POST',
     body: JSON.stringify(data),
+    headers: { 'Content-Type': 'application/json' },
   }).then((response) => response.text());
 
 export default {};
