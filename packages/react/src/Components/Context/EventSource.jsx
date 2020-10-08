1acb12e66b5b8a1fc4f809d1a39291436a927d8a
Ali Ali <alialbaker2007@gmail.com>
2020-10-08 23:30:49 +0100

    Remove old files

--- packages/react/src/Components/Context/EventSource.jsx
+++
@@ -1,9 +0,0 @@
-// Global imports
-import React from 'react';
-
-const EventSourceContext = React.createContext();
-
-const { Provider: EventSourceProvider } = EventSourceContext;
-const { Consumer: EventSourceConsumer } = EventSourceContext;
-
-export { EventSourceContext, EventSourceProvider, EventSourceConsumer };
--- packages/react/src/Components/Context/LivePhoto.jsx
+++
@@ -1,9 +0,0 @@
-// Global imports
-import React from 'react';
-
-const LivePhotoContext = React.createContext();
-
-const { Provider: LivePhotoProvider } = LivePhotoContext;
-const { Consumer: LivePhotoConsumer } = LivePhotoContext;
-
-export { LivePhotoContext, LivePhotoProvider, LivePhotoConsumer };
