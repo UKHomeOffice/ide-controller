12b0896bd91d3ae509d50756cb8454d5d8dbaa19
Ali Ali <alialbaker2007@gmail.com>
2020-10-11 15:40:21 +0100

    Fix main container width

--- packages/react/src/Components/Pages/index.jsx
+++ packages/react/src/Components/Pages/index.jsx
@@ -15,29 +15,27 @@ import { Header } from '../Organisms';
 const Index = () => (
   <>
     <Header />
-    <div className="govuk-width-container">
-      <main
-        className="govuk-main-wrapper govuk-main-wrapper--auto-spacing"
-        role="main"
-      >
-        <Row>
-          <Column size="one-quarter">
-            <MatchValue />
-            <ImageComparisonsTable />
-            <hr className="govuk-section-break govuk-section-break--m" />
-            <DataReadTable />
-            <p>
-              This data will automatically be deleted when another document is
-              scanned or after 15 minutes.
-            </p>
-            <Button buttonVariant="warning">Clear Data Now</Button>
-          </Column>
-          <Column size="three-quarters">
-            <MainSection />
-          </Column>
-        </Row>
-      </main>
-    </div>
+    <main
+      className="govuk-width-container govuk-main-wrapper--auto-spacing"
+      role="main"
+    >
+      <Row>
+        <Column size="one-quarter">
+          <MatchValue />
+          <ImageComparisonsTable />
+          <hr className="govuk-section-break govuk-section-break--m" />
+          <DataReadTable />
+          <p>
+            This data will automatically be deleted when another document is
+            scanned or after 15 minutes.
+          </p>
+          <Button buttonVariant="warning">Clear Data Now</Button>
+        </Column>
+        <Column size="three-quarters">
+          <MainSection />
+        </Column>
+      </Row>
+    </main>
   </>
 );
 
