caa97d3813f3a421e716ca08e7bcf13d3d0babc8
James <jpoulten@me.com>
2020-10-12 23:43:29 +0100

    WIP

--- packages/react/src/Components/Molecules/DataReadTable.jsx
+++ packages/react/src/Components/Molecules/DataReadTable.jsx
@@ -1,23 +1,40 @@
 // Global imports
-import React from 'react';
+import React, { useContext } from 'react';
 
 // Local imports
 import TableRow from './TableRow';
+import { EventSourceContext } from '../Context/EventSource';
+
+const chipReadText = (chipRead) => {
+  return chipRead ? 'successful' : 'No data';
+};
+const chipReadClassName = (chipRead) => {
+  return chipRead ? 'passed' : 'neutral';
+};
+
+const mrzReadText = (mrzRead) => {
+  return mrzRead ? 'successful' : 'No data';
+};
+const mrzReadClassName = (mrzRead) => {
+  return mrzRead ? 'passed' : 'neutral';
+};
 
 const DataReadTable = () => {
+  const { CD_SCDG1_CODELINE, CD_CODELINE } = useContext(EventSourceContext);
+
   return (
     <table className="govuk-table">
       <caption className="govuk-table__caption">Data read</caption>
       <tbody className="govuk-table__body">
         <TableRow
-          rowLabel="Live to chip"
-          tagStatus="neutral"
-          tagText="No Data"
+          rowLabel="Chip opened"
+          tagStatus={chipReadClassName(CD_SCDG1_CODELINE)}
+          tagText={chipReadText(CD_SCDG1_CODELINE)}
         />
         <TableRow
-          rowLabel="Live to document"
-          tagStatus="neutral"
-          tagText="No Data"
+          rowLabel="MRZ read"
+          tagStatus={mrzReadClassName(CD_CODELINE)}
+          tagText={mrzReadText(CD_CODELINE)}
         />
       </tbody>
     </table>
