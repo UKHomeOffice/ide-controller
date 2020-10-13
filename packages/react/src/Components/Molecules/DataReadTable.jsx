74300e96a9a1f7cde33147124798a414f392b2ae
James <jpoulten@me.com>
2020-10-13 11:21:46 +0100

    Add context to data read table

--- packages/react/src/Components/Molecules/DataReadTable.jsx
+++ packages/react/src/Components/Molecules/DataReadTable.jsx
@@ -5,36 +5,44 @@ import React, { useContext } from 'react';
 import TableRow from './TableRow';
 import { EventSourceContext } from '../Context/EventSource';
 
-const chipReadText = (chipRead) => {
-  return chipRead ? 'successful' : 'No data';
-};
-const chipReadClassName = (chipRead) => {
-  return chipRead ? 'passed' : 'neutral';
+const tagText = (data, chipData) => {
+  if (chipData) {
+    if (data.data === chipData.data) {
+      return 'successful';
+    }
+    return 'warning';
+  }
+  return data ? 'successful' : 'No data';
 };
 
-const mrzReadText = (mrzRead) => {
-  return mrzRead ? 'successful' : 'No data';
-};
-const mrzReadClassName = (mrzRead) => {
-  return mrzRead ? 'passed' : 'neutral';
+const tagClassName = (data, chipData) => {
+  if (chipData) {
+    if (data.data === chipData.data) {
+      return 'passed';
+    }
+    return 'warning';
+  }
+  return data ? 'passed' : 'neutral';
 };
 
 const DataReadTable = () => {
-  const { CD_SCDG1_CODELINE, CD_CODELINE } = useContext(EventSourceContext);
+  const { CD_SCDG1_CODELINE_DATA, CD_CODELINE_DATA } = useContext(
+    EventSourceContext
+  );
 
   return (
     <table className="govuk-table font--32">
       <caption className="govuk-table__caption">Data read</caption>
       <tbody className="govuk-table__body">
         <TableRow
-          rowLabel="Chip opened"
-          tagStatus={chipReadClassName(CD_SCDG1_CODELINE)}
-          tagText={chipReadText(CD_SCDG1_CODELINE)}
+          rowLabel="Open chip"
+          tagStatus={tagClassName(CD_SCDG1_CODELINE_DATA)}
+          tagText={tagText(CD_SCDG1_CODELINE_DATA)}
         />
         <TableRow
-          rowLabel="MRZ read"
-          tagStatus={mrzReadClassName(CD_CODELINE)}
-          tagText={mrzReadText(CD_CODELINE)}
+          rowLabel="Read MRZ"
+          tagStatus={tagClassName(CD_CODELINE_DATA, CD_SCDG1_CODELINE_DATA)}
+          tagText={tagText(CD_CODELINE_DATA, CD_SCDG1_CODELINE_DATA)}
         />
       </tbody>
     </table>
