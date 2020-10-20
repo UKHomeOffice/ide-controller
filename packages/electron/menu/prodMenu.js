6563b871cd431713283f443b9ffa22f2efdd9b4c
Ali Ali <alialbaker2007@gmail.com>
2020-10-20 22:06:54 +0100

    Apply cache fix

--- packages/electron/menu/prodMenu.js
+++ packages/electron/menu/prodMenu.js
@@ -1,3 +1,15 @@
-const prodMenu = [];
+const prodMenu = [
+  {
+    label: 'Developer',
+    submenu: [
+      {
+        role: 'services',
+      },
+      {
+        role: 'toggledevtools',
+      },
+    ],
+  },
+];
 
 module.exports = prodMenu;
