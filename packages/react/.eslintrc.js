483f9fe8f230fad2ffb3058218c46ee7e1b1a071
Ali Ali <alialbaker2007@gmail.com>
2020-10-20 10:32:31 +0100

    Eslint allow nested ternary

--- packages/react/.eslintrc.js
+++ packages/react/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
   plugins: ['react', 'prettier'],
   ignorePatterns: ['/node_modules', '/src/__tests__/__snapshots__', '/build'],
   rules: {
+    "no-nested-ternary": "off",
     "react/destructuring-assignment": "off",
     "jsx-a11y/media-has-caption": "off",
     quotes: ['error', 'single'],
