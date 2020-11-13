8d01e99e2a364c3e64dc5e5977de5b95c0f585d8
Ali Ali <alialbaker2007@gmail.com>
2020-11-13 15:22:27 +0000

    fix api-endpoint

--- packages/react/src/config/api-endpoints.js
+++ packages/react/src/config/api-endpoints.js
@@ -1,4 +1,4 @@
-const DATA_READER = 'http://localhost:1110/reader/data';
-const IMAGE_MATCH = 'http://localhost:1111/image/match';
+const DATA_READER = 'http://localhost:8080/reader/data';
+const IMAGE_MATCH = 'http://localhost:8081/image/match';
 
 export { DATA_READER, IMAGE_MATCH };
