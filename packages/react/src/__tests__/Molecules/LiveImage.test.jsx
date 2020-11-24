75390d60e9583942286c251d5c88ee58b1d3d410
Ali Ali <alialbaker2007@gmail.com>
2020-11-24 17:25:58 +0000

    Add tests

--- packages/react/src/__tests__/Molecules/LiveImage.test.jsx
+++ packages/react/src/__tests__/Molecules/LiveImage.test.jsx
@@ -29,4 +29,71 @@ describe('<LiveImage />', () => {
 
     expect(asFragment).toMatchSnapshot();
   });
+
+  it('Video is shown if showVideo is true', () => {
+    customRender(<LiveImage cameraId="123" />);
+    const video = screen.getByTestId('atoms-video');
+
+    expect(video).toBeInTheDocument();
+  });
+
+  it('CanvasRect is shown if showVideo is true', () => {
+    customRender(<LiveImage cameraId="123" />);
+    const canvasRect = screen.getByTestId('atoms-canvas-rect');
+
+    expect(canvasRect).toBeInTheDocument();
+  });
+
+  it('CanvasImage isnt shown if showCanvas is false', () => {
+    customRender(<LiveImage cameraId="123" />);
+    const canvasImage = screen.queryByTestId('atoms-canvas-image');
+
+    expect(canvasImage).toBeNull();
+  });
+
+  it('Video width prop are equal to livePhotoConfig.video.width', () => {
+    customRender(<LiveImage cameraIdId="123" />);
+    const video = screen.getByTestId('atoms-video');
+
+    expect(video).toHaveAttribute('width', String(livePhotoConfig.video.width));
+  });
+
+  it('Video height prop are equal to livePhotoConfig.video.width', () => {
+    customRender(<LiveImage cameraIdId="123" />);
+    const video = screen.getByTestId('atoms-video');
+
+    expect(video).toHaveAttribute(
+      'height',
+      String(livePhotoConfig.video.height)
+    );
+  });
+
+  it('Video renders properly className', () => {
+    customRender(<LiveImage cameraIdId="123" />);
+    const video = screen.getByTestId('atoms-video');
+
+    expect(video).toHaveClass('live-image__video');
+  });
+
+  it('CanvasRect width prop are equal to livePhotoConfig.video.width', () => {
+    customRender(<LiveImage cameraIdId="123" />);
+    const rect = screen.getByTestId('atoms-canvas-rect');
+
+    /* Note: useing livePhotoConfig.video.height only because we rotate the camera */
+    expect(rect).toHaveAttribute('width', String(livePhotoConfig.video.height));
+  });
+
+  it('CanvasRect height prop are equal to livePhotoConfig.video.width', () => {
+    customRender(<LiveImage cameraIdId="123" />);
+    const rect = screen.getByTestId('atoms-canvas-rect');
+
+    expect(rect).toHaveAttribute('height', String(livePhotoConfig.video.width));
+  });
+
+  it('CanvasRect renders properly className', () => {
+    customRender(<LiveImage cameraIdId="123" />);
+    const video = screen.getByTestId('atoms-canvas-rect');
+
+    expect(video).toHaveClass('live-image__canvas position-absolute');
+  });
 });
