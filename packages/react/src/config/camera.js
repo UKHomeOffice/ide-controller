cc308db82f439cd357a11891002022415a4a50fc
Ali Ali <alialbaker2007@gmail.com>
2020-12-03 12:20:58 +0000

    add canvas width & height

--- packages/react/src/config/camera.js
+++ packages/react/src/config/camera.js
@@ -4,6 +4,10 @@ export const livePhotoConfig = {
     height: 1080,
     sourceModel: 'C920',
   },
+  canvas: {
+    width: 405,
+    height: 617.5,
+  },
   threshold: 0.8,
   zoomFactor: 0.85,
   imageResolution: 40,
