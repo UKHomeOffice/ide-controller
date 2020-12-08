d3cce1f670747612a80d5f3dfc74db7d41732312
Ali Ali <alialbaker2007@gmail.com>
2020-12-08 16:55:58 +0000

    Refactor

--- packages/react/src/helpers/ipcMainEvents.js
+++ packages/react/src/helpers/ipcMainEvents.js
@@ -3,10 +3,10 @@ import { getCameraDevices } from './camera';
 const electron = window.require('electron');
 const { ipcRenderer } = electron;
 
-(async () => {
+export const sendCameraDevices = async () => {
   const cameraDevices = await getCameraDevices();
   ipcRenderer.send('webCamDevices', cameraDevices);
-})();
+};
 
 export const sendToElectronStore = (key, value) => {
   ipcRenderer.invoke('addToStore', key, value);
