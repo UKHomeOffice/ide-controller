61f46d506d422ef901e1613c915509c1d020bd08
Ali Ali <alialbaker2007@gmail.com>
2021-05-10 11:09:21 +0100

    Refactor devMenu

--- packages/electron/menu/devMenu.js
+++ packages/electron/menu/devMenu.js
@@ -1,5 +1,15 @@
 const readerServer = require('../../idemia-reader-mock');
 
+const triggerAction = (functionName) => {
+  const tryAgain = setInterval(() => {
+    const readerServerFunction = readerServer[functionName];
+    if (readerServerFunction) {
+      readerServerFunction();
+      clearInterval(tryAgain);
+    }
+  }, 1000);
+};
+
 const devMenu = [
   {
     label: 'Developer',
@@ -33,122 +43,52 @@ const devMenu = [
       {
         label: 'Trigger Scan Document With Chip',
         accelerator: 'F1',
-        click: () => {
-          const tryAgain = setInterval(() => {
-            if (readerServer.triggerWithChip) {
-              readerServer.triggerWithChip();
-              clearInterval(tryAgain);
-            }
-          }, 1000);
-        },
+        click: () => triggerAction('triggerWithChip'),
       },
       {
         label: 'Trigger Scan Document Without Chip',
         accelerator: 'F2',
-        click: () => {
-          const tryAgain = setInterval(() => {
-            if (readerServer.triggerWithoutChip) {
-              readerServer.triggerWithoutChip();
-              clearInterval(tryAgain);
-            }
-          }, 1000);
-        },
+        click: () => triggerAction('triggerWithoutChip'),
       },
       {
         label: 'Trigger Scan Document With Chip Slow',
         accelerator: 'F3',
-        click: () => {
-          const tryAgain = setInterval(() => {
-            if (readerServer.triggerWithChipSlow) {
-              readerServer.triggerWithChipSlow();
-              clearInterval(tryAgain);
-            }
-          }, 1000);
-        },
+        click: () => triggerAction('triggerWithChipSlow'),
       },
       {
         label: 'Trigger Scan Document With PACE',
         accelerator: 'F4',
-        click: () => {
-          const tryAgain = setInterval(() => {
-            if (readerServer.triggerWithoutChip) {
-              readerServer.triggerWithPACE();
-              clearInterval(tryAgain);
-            }
-          }, 1000);
-        },
+        click: () => triggerAction('triggerWithPACE'),
       },
       {
         label: 'Trigger Front Side ID Scan Document',
         accelerator: 'F5',
-        click: () => {
-          const tryAgain = setInterval(() => {
-            if (readerServer.triggerWithoutChip) {
-              readerServer.triggerWithIDFront();
-              clearInterval(tryAgain);
-            }
-          }, 1000);
-        },
+        click: () => triggerAction('triggerWithIDFront'),
       },
       {
         label: 'Trigger Back Side ID Scan Document',
         accelerator: 'F6',
-        click: () => {
-          const tryAgain = setInterval(() => {
-            if (readerServer.triggerWithoutChip) {
-              readerServer.triggerWithIDBack();
-              clearInterval(tryAgain);
-            }
-          }, 1000);
-        },
+        click: () => triggerAction('triggerWithIDBack'),
       },
       {
         label: 'Trigger Scan Document data00',
         accelerator: 'F7',
-        click: () => {
-          const tryAgain = setInterval(() => {
-            if (readerServer.triggerWithoutChip) {
-              readerServer.data00();
-              clearInterval(tryAgain);
-            }
-          }, 1000);
-        },
+        click: () => triggerAction('data00'),
       },
       {
         label: 'Trigger Scan Document data02',
         accelerator: 'F8',
-        click: () => {
-          const tryAgain = setInterval(() => {
-            if (readerServer.triggerWithoutChip) {
-              readerServer.data02();
-              clearInterval(tryAgain);
-            }
-          }, 1000);
-        },
+        click: () => triggerAction('data02'),
       },
       {
         label: 'Trigger Scan Document data04',
         accelerator: 'F9',
-        click: () => {
-          const tryAgain = setInterval(() => {
-            if (readerServer.triggerWithoutChip) {
-              readerServer.data04();
-              clearInterval(tryAgain);
-            }
-          }, 1000);
-        },
+        click: () => triggerAction('data04'),
       },
       {
         label: 'Trigger Scan Document MRZ Covered',
         accelerator: 'F10',
-        click: () => {
-          const tryAgain = setInterval(() => {
-            if (readerServer.triggerWithoutChip) {
-              readerServer.mrzCovered();
-              clearInterval(tryAgain);
-            }
-          }, 1000);
-        },
+        click: () => triggerAction('mrzCovered'),
       },
     ],
   },
